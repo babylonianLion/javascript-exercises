550c28cae2b40dacdf6aaa7aabae4e55810955b2
Hussain Al Zerjawi <hussain.alzerjawi@gmail.com>
2022-09-10 17:55:58 +0300

    Completed exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, number) {
+    let repeatedString = "";
+    if(number < 0){
+        return "ERROR";
+    }
+    for(let x = 0; x < number; x++){
+        repeatedString += text;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
