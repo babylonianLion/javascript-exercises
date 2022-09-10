da3e62c763f5794467378086ba87bd2ade43c2f6
Hussain Al Zerjawi <hussain.alzerjawi@gmail.com>
2022-09-10 18:06:54 +0300

    Completed Exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let reverseText = "";
+    for(let x = text.length; x >= 0; x--){
+        reverseText += text.charAt(x);
+    }
+    return reverseText;
 };
 
 // Do not edit below this line
