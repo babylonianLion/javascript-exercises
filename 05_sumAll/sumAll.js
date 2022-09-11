3a0b78318e76e471fb7cdf6c00e14d38c6a4e617
Hussain Al Zerjawi <hussain.alzerjawi@gmail.com>
2022-09-11 15:31:00 +0300

    Completed Sum All exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(start, end) {
+    let sum = 0;
+    if(start > end){
+        start = start + end;
+        end = start - end;
+        start = start - end;
+    }
+    if(start < 0 || end < 0 || Number.isInteger(start) == false || Number.isInteger(end) == false){
+        return "ERROR";
+    }
+    for (let x = start; x <= end; x++){
+        sum += x;
+    }
+    return sum;
 };
 
 // Do not edit below this line
