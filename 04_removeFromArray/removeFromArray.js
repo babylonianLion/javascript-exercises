8e6cdeb28c5f322a5ab374e339f9562b286cca17
Hussain Al Zerjawi <hussain.alzerjawi@gmail.com>
2022-09-11 15:17:58 +0300

    Completed Remove From Array exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    const fixedArray = [];
+    for(item of array){
+        for(let x = 1; x < arguments.length; x++){
+            if (item === arguments[x]){
+                break;
+            }
+            else if (item != arguments[x] && x == arguments.length -1){
+                fixedArray.push(item);
+            }
+        }
+    }
+    console.log(fixedArray);
+    return fixedArray;
 };
 
 // Do not edit below this line
